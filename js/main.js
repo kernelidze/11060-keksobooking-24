640e9c56d9ea8885db29265d9804b0300567ea15
kernelidze <kernelidze@gmail.com>
2021-10-07 02:22:04 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function getRandomIntegerNumber(min, max) {
   if (min < 0 || max <= 0 ) {
-    return;
+    return ('Использован неверный диапазон значений');
   }
   if (min > max) {
     [min, max] = [max, min];
@@ -10,33 +10,38 @@ function getRandomIntegerNumber(min, max) {
 
 function getRandomNumber(min, max, numberOfSigns) {
   if (min < 0 || max <0 ) {
-    return;
+    return ('Использован неверный диапазон значений');
   }
   if (min > max) {
     [min, max] = [max, min];
   }
-  const NUMBER = Math.random() * (max - min + 1) + min;
+  const NUMBER = Math.random() * (max - min) + min;
   const FIXED_NUMBER = Number(NUMBER.toFixed(numberOfSigns));
   return (FIXED_NUMBER);
 }
 
+const avatarNumbers = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
 const houseTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const checkin = ['12:00', '13:00', '14:00'];
 const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
 function getRandomArrayElement(array) {
-  const randomArrayElement = Math.floor(Math.random() * array.length);
+  const randomArrayElement = array[Math.floor(Math.random() * array.length)];
   return randomArrayElement;
 }
 
-const userType = function () {
+const getAdsDescription = function () {
   const author = {
-    avatar: avatarLink,
+    avatar: `img/avatars/user${(getRandomArrayElement(avatarNumbers))}.png`,
+  };
+  const location = {
+    lat: getRandomNumber(35.65000, 35.70000, 5),
+    lng: getRandomNumber(139.70000, 139.80000, 5),
   };
   const offer = {
     title: 'Добро пожаловать в выбранный вами дом',
-    address: location,
+    address: `Адрес: широта ${location.lat} и долгота ${location.lng}`,
     price: getRandomIntegerNumber(0, 1000),
     type: getRandomArrayElement(houseTypes),
     rooms: getRandomIntegerNumber(0, 10),
@@ -46,9 +51,12 @@ const userType = function () {
     features: getRandomArrayElement(features),
     description: 'Описание дома',
     photos: getRandomArrayElement(photos),
-    location: {
-      lat: getRandomNumber(35.65000, 35.70000, 5),
-      lng: getRandomNumber(139.70000, 139.80000, 5),
-    },
   };
+  const objectAssign = Object.assign({}, author, location, offer);
+  const adsDescriptionArray = Object.values(objectAssign);
+  //console.log(adsDescriptionArray);
+  return adsDescriptionArray;
 };
+
+getAdsDescription();
+console.log(getAdsDescription());
