89ee2e9faf966b61e72790da277b4014ca03b2de
kernelidze <kernelidze@gmail.com>
2021-10-21 02:42:38 +0300

    Отрисуй меня полностью (часть 1)

--- js/data.js
+++ js/data.js
@@ -1,7 +1,45 @@
+import {getRandomIntegerNumber, getRandomNumber, getRandomArrayElement, getRandomRangeFromArray} from './util.js';
+
 const avatarNumbers = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
 const houseTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const checkin = ['12:00', '13:00', '14:00'];
 const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
-export {avatarNumbers, houseTypes, checkin, features, photos};
+
+const getAdsDescription = function () {
+  const author = {
+    avatar: `img/avatars/user${(getRandomArrayElement(avatarNumbers))}.png`,
+  };
+  const location = {
+    lat: getRandomNumber(35.65000, 35.70000, 5),
+    lng: getRandomNumber(139.70000, 139.80000, 5),
+  };
+  const offer = {
+    title: 'Добро пожаловать в выбранный вами дом',
+    address: `${location.lat} ${location.lng}`,
+    price: getRandomIntegerNumber(0, 1000),
+    type: getRandomArrayElement(houseTypes),
+    rooms: getRandomIntegerNumber(0, 10),
+    guests: getRandomIntegerNumber(1, 100),
+    checkin: getRandomArrayElement(checkin),
+    checkout: getRandomArrayElement(checkin),
+    features: getRandomRangeFromArray(features),
+    description: 'Описание дома',
+    photos: getRandomRangeFromArray(photos),
+  };
+  return {author, location, offer};
+};
+
+const getUsersAdsArray = function () {
+  const userAdsArray = [];
+  for (let i = 0; i < 10; i++) {
+    userAdsArray[i] = getAdsDescription();
+  }
+  return userAdsArray;
+};
+
+getUsersAdsArray();
+
+export {getUsersAdsArray};
+
---
+++ js/similar-elements.js
@@ -0,0 +1,49 @@
+import {getUsersAdsArray} from './data.js';
+
+const TYPE_NAME = {flat: 'Квартира', bungalow: 'Бунгало', house: 'Дом', palace: 'Дворец', hotel: 'Отель'};
+
+const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
+const mapCanvas = document.querySelector('#map-canvas');
+const fragment = document.createDocumentFragment();
+
+const generateFragment = (element, item, currenFragment) => {
+  element.querySelector('.popup__title').textContent = item.offer.title;
+  element.querySelector('.popup__text--address').textContent = item.offer.address;
+  element.querySelector('.popup__text--price').textContent =`${item.offer.price} ₽/ночь`;
+  element.querySelector('.popup__type').textContent = TYPE_NAME[item.offer.type];
+  element.querySelector('.popup__text--capacity').textContent = `${item.offer.rooms} комнаты для ${item.offer.guests} гостей`;
+  element.querySelector('.popup__text--time').textContent = `Заезд после ${item.offer.checkin}, выезд до ${item.offer.checkout}`;
+  const arrayFeatures = item.offer.features;
+  const featuresContainer = element.querySelector('.popup__features');
+  featuresContainer.innerHTML = '';
+  arrayFeatures.forEach((feature) => {
+    const featuresListItem = document.createElement('li');
+    featuresListItem.classList.add('popup__feature');
+    featuresListItem.classList.add(`popup__feature--${feature}`);
+    element.querySelector('.popup__features').appendChild(featuresListItem);
+  });
+  element.querySelector('.popup__description').textContent = item.offer.description;
+  const hollowPhoto = element.querySelector('.popup__photos');
+  hollowPhoto.innerHTML = '';
+  item.offer.photos.forEach((photo) => {
+    const photoElement = document.createElement('img');
+    photoElement.src = photo;
+    photoElement.width = 45;
+    photoElement.height = 40;
+    photoElement.alt = 'Фотография жилья';
+    photoElement.classList.add('popup__photo');
+    element.querySelector('.popup__photos').appendChild(photoElement);
+  });
+  element.querySelector('.popup__avatar').src = item.author.avatar;
+  currenFragment.appendChild(element);
+};
+
+const userAdsArray = getUsersAdsArray();
+userAdsArray.forEach((item) => {
+  const element1 = cardTemplate.cloneNode(true);
+  generateFragment(element1, item, fragment);
+});
+
+mapCanvas.appendChild(fragment.children[1]);
+
+
